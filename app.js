3b4a33a5d5ac1c9334c574e9134108095fd44673
Viv <hvivian2012@yahoo.com>
2020-04-09 13:26:54 -0700

    toDos Array

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-function onReady() {
+function onReady(){
+  let toDos = [];
+
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
   const NEW_TODO_TEXT = document.getElementById('newToDoText');
   const TODO_LIST = document.getElementbyId('toDoList');
 
-  ADD_TODO_FORM.addEventListener('submit', () => {
-
+  ADD_TODO_FORM.addEventListener('submit', (event) => {
     // avoids redirecting user to a new page
     event.preventDefault();
 
     // get the text
     let title = NEW_TODO_TEXT.value;
 
+    toDos.push(title);
+    console.log(toDos);
+
+    toDos.push(toDoObj)
+
     // create a new li
     let newLi = document.createElement('li');
-
     // create a new input
     let checkbox = document.createElement('input');
-
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
@@ -38,7 +42,7 @@ function onReady() {
     // empty input
     NEW_TODO_TEXT.value = '';
   });
-}
+};
 
 window.onload = function() {
   onReady();
