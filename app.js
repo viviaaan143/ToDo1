b831b2f6f2440cbc82e3074e97537b71f93e8c27
Viv <hvivian2012@yahoo.com>
2020-04-09 11:05:07 -0700

    added code

---
+++ app.js
@@ -0,0 +1,45 @@
+function onReady() {
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  const NEW_TODO_TEXT = document.getElementById('newToDoText');
+  const TODO_LIST = document.getElementbyId('toDoList');
+
+  ADD_TODO_FORM.addEventListener('submit', () => {
+
+    // avoids redirecting user to a new page
+    event.preventDefault();
+
+    // get the text
+    let title = NEW_TODO_TEXT.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+    // delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event){
+      TODO_LIST.removeChild(this.parentElement);
+    })
+
+    // set the title
+    newLi.textContent = title;
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
+    // attach the li to the ul
+    TODO_LIST.appendChild(newLi);
+    // empty input
+    NEW_TODO_TEXT.value = '';
+  });
+}
+
+window.onload = function() {
+  onReady();
+};
